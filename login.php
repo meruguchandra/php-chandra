bbd072d0597c9d56704bdef52c4a497faeb2470c
chandra sekhar reddy merugu <meruguchandra@gmail.com>
2021-06-06 10:19:12 +0800

    Update login.php

--- login.php
+++ login.php
@@ -26,6 +26,9 @@
 
 
 
+
+
+
 <form name="loginform" method="POST" action="login.php">
 username:<input type="text" name='uname' value=<?php if(isset($_POST["uname"])) echo $_POST["uname"] ; ?>><br><br> 
 <!--		username:<input type="text" name='uname'><br><br>-->
